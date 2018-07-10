d0092d17d1ef6e88f0e747aa7e1709dc2cecc858
Damien Golding <noreply@damiengolding.com>
2018-07-10 20:18:05 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# Description
+
+Mock library for getUserMedia and related functions.
+
+## Usage
+
+The code below will automatically apply mocks to the following functions:
+
+* navigator.getUserMedia
+* navigator.mediaDevices.getUserMedia
+* navigator.mediaDevices.enumerateDevices
+* getSupportedConstraints
+
+```
+window.getUserMediaMock = new GetUserMediaMock();
+getUserMediaMock.mock();
+```
+
+For more advanced usage check [Functions].
+
+## Functions
+
+TO DO(Check code for now)
+
+## Dependencies
+
+https://github.com/meenie/band.js MIT
+http://freemusicarchive.org/music/Tequila_Moonrise/Best_Of__Pick_Your_Player/Tequila_Moonrise_-_Best_Of_-_Pick_Your_Player_-_09_Tequila_Moonrise Public Domain
+https://videos.pexels.com/videos/video-of-people-walking-855564 CC0 License
---
+++ index.js
@@ -0,0 +1,344 @@
+/**
+ * Class for creating mock of getUserMedia for navigator.getUserMedia and navigator.mediaDevice.getUserMedia.
+ * Usage: const m = new GetUserMediaMock(); m.setup();
+ */
+class GetUserMediaMock{
+
+    constructor(){
+        this.VIDEO_URL = './media/708213662.mp4';
+        this.AUDIO_URL = './media/Tequila_Moonrise_-_09_-_Tequila_Moonrise.mp3';
+        this.settings = {
+            mockType: 'canvas',//canvas, mediaElement, audioContext
+            constraints: {
+                video: {
+                    aspectRatio: false,//Upon testing in Chrome, width and height hold priority over aspectRatio.
+                    facingMode: false,
+                    frameRate: false,
+                    height: false,
+                    width: false
+                },
+        
+                audio: {
+                    autoGainControl: false,
+                    channelCount: false,
+                    echoCancellation: false,
+                    latency: false,
+                    noiseSuppression: false,
+                    sampleRate: false,
+                    sampleSize: false,
+                    volume: false
+                },
+        
+                image: {
+                    whiteBalanceMode: false,
+                    exposureMode: false,
+                    focusMode: false,
+                    pointsOfInterest: false,
+                    exposureCompensation: false,
+                    colorTemperature: false,
+                    iso: false,
+                    brightness: false,
+                    contrast: false,
+                    saturation: false,
+                    sharpness: false,
+                    focusDistance: false,
+                    zoom: false,
+                    torch: false
+                }
+            }
+        };
+        this.state = {
+            prepared: false
+        };
+    }
+
+    _storeOldHandles(){
+        navigator._getUserMedia = navigator.getUserMedia
+        navigator._mediaDevices = {};
+        if(!navigator.mediaDevices){
+            navigator.mediaDevices = {};
+        }
+        const m = navigator._mediaDevices;
+        m.enumerateDevices = navigator.mediaDevices.enumerateDevices;
+        m.getSupportedConstraints = navigator.mediaDevices.getSupportedConstraints;
+        m.getUserMedia = navigator.mediaDevices.getUserMedia;
+
+        this.state.prepared = true;
+    }
+
+    /**
+     * Dynamically update constraints. Applied on next call of getUserMedia, etc.
+     * @param {String} type any key in this.settings.constraints
+     * @param {Object} updates Data to apply to constraints
+     * @param {Boolean} overwrite Whether to fully overwrite original.
+     */
+    updateConstraints(type='video', updates={}, overwrite=false){
+        const c = this.settings.constraints;
+        if(!c[type]){return false;}
+        if(overwrite){
+            c[type] = {};
+        }
+        for(let key in updates){
+            c[type][key] = updates[key];
+        }
+        return this;
+    }
+
+    /**
+     * Applies mock to environment.
+     * Generally should be applied before other scripts once.
+     * @param {Object} options Way to only mock certain features. Mocks all by default.
+     * @return this
+     */
+    mock(options){
+        if(typeof options !== 'object'){
+            options = {
+                getUserMedia: true,
+                mediaDevices: {
+                    getUserMedia: true,
+                    getSupportedConstraints: true,
+                    enumerateDevices: true
+                }
+            };
+        }
+
+        if(!this.state.prepared){
+            this._storeOldHandles();
+        }
+
+        //navigator.getUserMedia
+        if(options.getUserMedia){
+            navigator.getUserMedia = (constraints, onSuccess, onError)=>{
+                return this.getMockStreamFromConstraints(constraints)
+                .then(onSuccess)
+                .catch(onError);
+            }
+        }
+
+        if(options.mediaDevices){
+            
+            //navigator.mediaDevices.getUserMedia
+            if(options.mediaDevices.getUserMedia){
+                navigator.mediaDevices.getUserMedia = (constraints)=>{
+                    return this.getMockStreamFromConstraints(constraints);
+                }
+            }
+
+            //navigator.mediaDevices.getSupportedConstraints
+            if(options.mediaDevices.getSupportedConstraints){
+                navigator.mediaDevices.getSupportedConstraints = ()=>{
+                    return this.settings.constraints;
+                }
+            }
+
+            //navigator.mediaDevices.enumerateDevices
+            if(options.mediaDevices.enumerateDevices){
+                navigator.mediaDevices.enumerateDevices = ()=>{
+                    return this.getMockDevices();
+                }
+            }
+        }
+
+        return this;
+    }
+
+    /**
+     * Restores actually native handles if mock handles already applied.
+     */
+    restoreOldHandles(){
+        if(navigator._getUserMedia){
+            navigator.getUserMedia = navigator._getUserMedia;
+            navigator._getUserMedia = null;
+        }
+        if(navigator._mediaDevices){
+            navigator.mediaDevices.enumerateDevices = navigator._mediaDevices.enumerateDevices;
+            navigator.mediaDevices.getSupportedConstraints = navigator._mediaDevices.getSupportedConstraints;
+            navigator.mediaDevices.getUserMedia = navigator._mediaDevices.getUserMedia;
+            navigator._mediaDevices = null;
+        }
+
+        this.state.prepared = false;
+    }
+
+    /**
+     * Gets a media stream with motion and color.
+     * @see https://developer.mozilla.org/en-US/docs/Web/API/MediaTrackConstraints
+     * @param {Object} constraints 
+     * @return {Promise}
+     */
+    getMockStreamFromConstraints(constraints){
+        /*
+        Ways to create stream:
+        https://developer.mozilla.org/en-US/docs/Web/API/HTMLCanvasElement/captureStream
+        https://developer.mozilla.org/en-US/docs/Web/API/HTMLMediaElement/captureStream
+        https://developer.mozilla.org/en-US/docs/Web/API/AudioContext/createMediaStreamDestination
+        */
+
+        let stream = null;
+        const mockType = this.settings.mockType;
+        if(mockType === 'canvas'){
+            stream = this.getMockCanvasStream(constraints);
+        }
+        else if(mockType === 'mediaElement'){
+            stream = this.getMockMediaElementStream(constraints);
+        }
+        else if(mockType === 'audioContext'){
+            stream = this.getMockAudioContextStream(constraints);
+        }
+
+        return Promise.resolve(stream);
+    }
+
+    /**
+     * Returns stream that is internally generated using canvas and random data.
+     * @param {MediaTrackContraints} constraints 
+     * @return {MediaStream}
+     */
+    getMockCanvasStream(constraints){
+        const canvas = document.createElement('canvas');
+        canvas.width = this.getConstraint(constraints, 'video', 'width');
+        canvas.height = this.getConstraint(constraints, 'video', 'height');
+        const interval = this.createRandomCanvasDrawerInterval(canvas);
+        const stream = canvas.captureStream(this.getConstraint(constraints, 'video', 'frameRate'));
+        stream.stop = ()=>{
+            window.clearInterval(interval);
+            if(stream._stop){stream._stop();}//??
+        };
+        return stream;
+    }
+
+    /**
+     * Returns stream with media used as source.
+     * @param {MediaTrackContraints} constraints 
+     * @return {MediaStream}
+     */
+    getMockMediaElementStream(constraints){
+        const video = document.createElement('video');
+        video.autoplay = true;
+        video.loop = true;
+        
+        video.src = (!!constraints.video) ? this.VIDEO_URL : this.AUDIO_URL;
+
+        const stream = video.captureStream();
+        return stream;
+    }
+
+    /**
+     * Returns stream with audio
+     * @param {MediaTrackContraints} constraints 
+     * @return {MediaStream}
+     */
+    getMockAudioContextStream(constraints){
+        var conductor = new BandJS();
+        conductor.setTimeSignature(4,4);
+        conductor.setTempo(120);
+        var piano = conductor.createInstrument('square');
+        
+        piano.note('quarter', 'C4');
+        piano.note('quarter', 'D4');
+        piano.note('quarter', 'E4');
+        piano.note('quarter', 'F4');
+        
+        var player = conductor.finish();
+        player.loop(true);
+        
+        player.play();
+
+        const streamDestination = conductor.audioContext.createMediaStreamDestination();
+        const stream = streamDestination.stream;
+
+        return stream;
+    }
+
+    /**
+     * Creates and starts an interval that paints randomly to a canvas.
+     * @param {DOMCanvas} canvas 
+     * @return {Number} interval that can be cleared with window.clearInterval
+     */
+    createRandomCanvasDrawerInterval(canvas){
+        const FPS = 2;
+        const ms = 1000 / FPS;
+        const getRandom = (max)=>{
+            return Math.floor(Math.random() * max);
+        };
+        const interval = window.setInterval(()=>{
+            const ctx = canvas.getContext('2d');
+            
+            const x = 0;
+            const y = 0;
+            const width = getRandom(canvas.width);
+            const height = getRandom(canvas.height);
+
+            const r = getRandom(255);
+            const g = getRandom(255);
+            const b = getRandom(255);
+
+            ctx.fillStyle = `rgb(${r},${g},${b})`;
+
+            ctx.fillRect(x, y, width, height);
+        }, ms);
+
+        return interval;
+    }
+
+    /**
+     * Gets constraint by necessary identifiers.
+     * Returns appropriate defaults where important.
+     * @param {MediaTrackConstraints} constraints 
+     * @param {String} type 
+     * @param {String} key 
+     * @return {*}
+     */
+    getConstraint(constraints, type, key){
+        const subConstraints = (typeof constraints[type] === 'object') ? constraints[type] : {};
+        let value = subConstraints[key];
+        
+        //Defaults
+        if(key === 'width' && !value){
+            value = 640; 
+        }
+        if(key === 'height' && !value){
+            value = 480; 
+        }
+        if(key === 'frameRate' && !value){
+            value = 15; 
+        }
+
+        return value;
+    }
+    
+    /**
+     * Returns a set of mock devices using similar format.
+     * @return {Promise} resolves array
+     */
+    getMockDevices(){
+        const devices = [
+            {
+                kind: 'audioinput',
+                label: '(4- BUFFALO BSW32KM03 USB PC Camera)'
+            },
+            {
+                kind: 'audiooutput',
+                label: 'Bluetooth Hands-free Audio'
+            },
+            {
+                kind: 'videooutput',
+                label: 'BUFFALO BSW32KM03 USB PC Camera'
+            }
+        ];
+        return new Promise((resolve)=>{
+            devices.forEach((device, index)=>{
+                device.deviceId = String(index);
+                device.groupId = String(index);
+            });
+            return resolve(devices);
+        });
+    }
+}
+
+if(typeof window === 'object'){
+    window.GetUserMediaMock = GetUserMediaMock;
+}
+if(typeof module === 'object'){
+    module.exports = GetUserMediaMock;
+}
